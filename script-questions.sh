86093b594c61177d369614b040c26224b5820323
Nikita Baranov <nikita.baranov.devops@gmail.com>
2025-03-01 01:34:08 +0300

    script-questions.sh: fixing message dublication by cutting out the unused fields in json

--- script-questions.sh
+++ script-questions.sh
@@ -27,7 +27,8 @@ CURRENT_FILEPATH=qsts
 CURRENT_FILENAME=$DATETIME.json
 mkdir -p $CURRENT_FILEPATH
 
-curl -s --location --request GET 'https://feedbacks-api.wb.ru/api/v1/questions?isAnswered=false&take=5000&skip=0' --header 'Authorization: '$WB_TOKEN --header 'Content-Type: application/json' | jq -r '.data.questions' > $CURRENT_FILEPATH/$CURRENT_FILENAME
+curl -s --location --request GET 'https://feedbacks-api.wb.ru/api/v1/questions?isAnswered=false&take=5000&skip=0' --header 'Authorization: '$WB_TOKEN --header 'Content-Type: application/json' > $CURRENT_FILEPATH/$CURRENT_FILENAME
+echo $(jq -r '.data.questions' $CURRENT_FILEPATH/$CURRENT_FILENAME | jq -r 'del(.[].answer,.[].state,.[].wasViewed,.[].isWarned,.[].productDetails.size,.[].productDetails.nmId,.[].productDetails.imtId,.[].productDetails.productName,.[].productDetails.supplierName,.[].productDetails.brandName)' | jq -r '. |= sort_by(.createdDate)') > $CURRENT_FILEPATH/$CURRENT_FILENAME
 
     NOT_ANSWERED_JSON=$(<$CURRENT_FILEPATH/$CURRENT_FILENAME)
     if [ "$NOT_ANSWERED_JSON" != "[]" ] && [ "$NOT_ANSWERED_JSON" != null ] && [ "$NOT_ANSWERED_JSON" != "" ]; then
@@ -53,13 +54,15 @@ curl -s --location --request GET 'https://feedbacks-api.wb.ru/api/v1/questions?i
             do
                   echo j=$j
                   JSON_TEXT=$(jq -r '.['$j'].text' $CURRENT_FILEPATH/$CURRENT_FILENAME)
-                  JSON_DATE=$(jq -r '.['$j'].createdDate' $CURRENT_FILEPATH/$CURRENT_FILENAME)
+                  JSON_DATE_GMT=$(jq -r '.['$j'].createdDate' $CURRENT_FILEPATH/$CURRENT_FILENAME)
+                  JSON_DATE=$(TZ=Europe/Moscow date -d "$JSON_DATE_GMT" +'%Y-%m-%d %H:%M:%S')
+                  JSON_SKU=$(jq -r '.['$j'].productDetails.supplierArticle' $CURRENT_FILEPATH/$CURRENT_FILENAME)
                   # send messages from bot
                   for k in `seq 1 $CHAT_NUM`; do
                         current_chat_id=chat_id_$k
                         if  [ "${!current_chat_id}" != "" ]; then
 	                	curl -s -X POST 'https://api.telegram.org/bot'$TG_BOT_TOKEN'/sendMessage' -d chat_id=${!current_chat_id} \
-             	                        -d text="Новый вопрос! %0A$LK_NAME%0AТекст: $JSON_TEXT" &
+             	                        -d text="Новый вопрос! %0A$LK_NAME %0AАртикул: $JSON_SKU %0AДата: $JSON_DATE %0AТекст: $JSON_TEXT" &
                                 echo "--------------------"
                         fi
                   done
