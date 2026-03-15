acd4b3da3b831000e6a73f0b19d6c007fb077a46
Никита Баранов <baranov.nv@betcity.ru>
2026-03-16 00:39:39 +0300

    script-questions.sh: dublicate messages when answering fixed

--- script-questions.sh
+++ script-questions.sh
@@ -31,55 +31,58 @@ mkdir -p $CURRENT_FILEPATH
 curl -s --location --request GET 'https://feedbacks-api.wildberries.ru/api/v1/questions?isAnswered=false&take=5000&skip=0' --header 'Authorization: '$WB_TOKEN --header 'Content-Type: application/json' > $CURRENT_FILEPATH/$CURRENT_FILENAME
 echo $(jq -r '.data.questions' $CURRENT_FILEPATH/$CURRENT_FILENAME | jq -r 'del(.[].answer,.[].state,.[].wasViewed,.[].isWarned,.[].productDetails.size,.[].productDetails.nmId,.[].productDetails.imtId,.[].productDetails.productName,.[].productDetails.supplierName,.[].productDetails.brandName)' | jq -r '. |= sort_by(.createdDate)') > $CURRENT_FILEPATH/$CURRENT_FILENAME
 
-    NOT_ANSWERED_JSON=$(<$CURRENT_FILEPATH/$CURRENT_FILENAME)
-    if [ "$NOT_ANSWERED_JSON" != "[]" ] && [ "$NOT_ANSWERED_JSON" != null ] && [ "$NOT_ANSWERED_JSON" != "" ]; then
-        echo "New questions detected!"
+NOT_ANSWERED_JSON=$(<$CURRENT_FILEPATH/$CURRENT_FILENAME)
+if [ "$NOT_ANSWERED_JSON" != "[]" ] && [ "$NOT_ANSWERED_JSON" != null ] && [ "$NOT_ANSWERED_JSON" != "" ]; then
+    echo "New questions detected!"
 
-        FILE_IS_FIRST=$(ls -1 $CURRENT_FILEPATH | wc -l)
-        if [ $FILE_IS_FIRST -eq 1 ]; then
-            echo "This is the first run"
-            FILES_ARE_DIFFERENT=0
-        else
-            echo "This is not the first run"
-            PREVIOUS_FILENAME=$(ls -1 $CURRENT_FILEPATH | sort | tail -n 2 | head -n 1)
-            echo PREVIOUS_FILENAME=$PREVIOUS_FILENAME
-            FILES_ARE_DIFFERENT=$(jd -set $CURRENT_FILEPATH/$PREVIOUS_FILENAME $CURRENT_FILEPATH/$CURRENT_FILENAME | wc -l)
-            echo FILES_ARE_DIFFERENT=$FILES_ARE_DIFFERENT
-        fi
-
-
-        if [ $FILES_ARE_DIFFERENT -gt 0 ] && [ $FILE_IS_FIRST -gt 1 ]; then
-	    CHANGES_COUNT=$(jq length $CURRENT_FILEPATH/$CURRENT_FILENAME)
-            echo CHANGES_COUNT=$CHANGES_COUNT
-            for j in `seq 0 $(( $CHANGES_COUNT - 1 ))`
-            do
-                  echo j=$j
-                  JSON_TEXT=$(jq -r '.['$j'].text' $CURRENT_FILEPATH/$CURRENT_FILENAME)
-                  JSON_DATE_GMT=$(jq -r '.['$j'].createdDate' $CURRENT_FILEPATH/$CURRENT_FILENAME)
-                  JSON_DATE=$(TZ=Europe/Moscow date -d "$JSON_DATE_GMT" +'%Y-%m-%d %H:%M:%S')
-                  JSON_SKU=$(jq -r '.['$j'].productDetails.supplierArticle' $CURRENT_FILEPATH/$CURRENT_FILENAME)
-                  # send messages from bot
-                  for k in `seq 1 $CHAT_NUM`; do
-                        current_chat_id=chat_id_$k
-                        if  [ "${!current_chat_id}" != "" ]; then
-	                	curl -s $TG_PROXY -X POST 'https://api.telegram.org/bot'$TG_BOT_TOKEN'/sendMessage' -d chat_id=${!current_chat_id} \
-             	                        -d text="Новый вопрос! %0A$LK_NAME %0AАртикул: $JSON_SKU %0AДата: $JSON_DATE %0AТекст: $JSON_TEXT" &
-                                echo "--------------------"
-                        fi
-                  done
-            done
-# FIRST RUN EXCEPTION
-        elif [ $FILES_ARE_DIFFERENT -eq 0 ] && [ $FILE_IS_FIRST -eq 1 ]; then
-             echo "FIRST RUN! Creating base for future comparison"
-        elif [ $FILES_ARE_DIFFERENT -eq 0 ] && [ $FILE_IS_FIRST -gt 1 ]; then
-            echo "NO CHANGES FOUND!"
-            rm $CURRENT_FILEPATH/$CURRENT_FILENAME
-        fi
-        echo "--------------------"
-
+    FILE_IS_FIRST=$(ls -1 $CURRENT_FILEPATH | wc -l)
+    if [ $FILE_IS_FIRST -eq 1 ]; then
+        echo "This is the first run"
+        FILES_ARE_DIFFERENT=0
     else
-        echo "No new questions"
-        echo "--------------------"
-	rm $CURRENT_FILEPATH/$CURRENT_FILENAME
+        echo "This is not the first run"
+        PREVIOUS_FILENAME=$(ls -1 $CURRENT_FILEPATH | sort | tail -n 2 | head -n 1)
+        echo PREVIOUS_FILENAME=$PREVIOUS_FILENAME
+        FILES_ARE_DIFFERENT=$(jd -set $CURRENT_FILEPATH/$PREVIOUS_FILENAME $CURRENT_FILEPATH/$CURRENT_FILENAME | wc -l)
+        echo FILES_ARE_DIFFERENT=$FILES_ARE_DIFFERENT
     fi
 
+    if [ $FILES_ARE_DIFFERENT -gt 0 ] && [ $FILE_IS_FIRST -gt 1 ]; then
+        CHANGES_COUNT=$(jq length $CURRENT_FILEPATH/$CURRENT_FILENAME)
+        echo CHANGES_COUNT=$CHANGES_COUNT
+        for j in `seq 0 $(( $CHANGES_COUNT - 1 ))`; do
+            echo j=$j
+            JSON_ID=$(jq -r '.['$j'].id' $CURRENT_FILEPATH/$CURRENT_FILENAME)
+            JSON_TEXT=$(jq -r '.['$j'].text' $CURRENT_FILEPATH/$CURRENT_FILENAME)
+            JSON_DATE_GMT=$(jq -r '.['$j'].createdDate' $CURRENT_FILEPATH/$CURRENT_FILENAME)
+            JSON_DATE=$(TZ=Europe/Moscow date -d "$JSON_DATE_GMT" +'%Y-%m-%d %H:%M:%S')
+            JSON_SKU=$(jq -r '.['$j'].productDetails.supplierArticle' $CURRENT_FILEPATH/$CURRENT_FILENAME)
+            JSON_SKU=$(sed 's|\&||g' <<<$JSON_SKU)
+            # check if current question id exists in previous qsts reference file
+            QUESTION_ID_EXIST=$(grep -Rlw $CURRENT_FILEPATH/$PREVIOUS_FILENAME -e "$JSON_ID" | wc -l)
+            if [ $QUESTION_ID_EXIST -eq 0 ]; then
+                # send messages from bot
+                for k in `seq 1 $CHAT_NUM`; do
+                    current_chat_id=chat_id_$k
+                    if  [ "${!current_chat_id}" != "" ]; then
+                        curl -s $TG_PROXY -X POST 'https://api.telegram.org/bot'$TG_BOT_TOKEN'/sendMessage' -d chat_id=${!current_chat_id} \
+                                -d text="Новый вопрос! %0A$LK_NAME %0AАртикул: $JSON_SKU %0AДата: $JSON_DATE %0AТекст: $JSON_TEXT" &
+                        echo "--------------------"
+                    fi
+                done
+            else
+                echo "This question was already sent earlier"
+                echo "--------------------"
+            fi
+        done
+# FIRST RUN EXCEPTION
+    elif [ $FILES_ARE_DIFFERENT -eq 0 ] && [ $FILE_IS_FIRST -eq 1 ]; then
+        echo "FIRST RUN! Creating base for future comparison"
+    elif [ $FILES_ARE_DIFFERENT -eq 0 ] && [ $FILE_IS_FIRST -gt 1 ]; then
+        echo "NO CHANGES FOUND!"
+        rm $CURRENT_FILEPATH/$CURRENT_FILENAME
+    fi
+else
+    echo "No new questions"
+    rm $CURRENT_FILEPATH/$CURRENT_FILENAME
+fi
