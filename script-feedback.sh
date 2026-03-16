c3d3951e8ce86df7289627712d743c039c36b523
n-baranov <90695238+n-baranov@users.noreply.github.com>
2026-03-17 02:23:56 +0300

    Feature images (#3)
    minor improvements

--- script-feedback.sh
+++ script-feedback.sh
@@ -26,9 +26,18 @@ LENGTH=$(jq length sku.json)
 DATETIME=$(date +'%Y-%m-%d-%H-%M-%S')
  
 if [ ! -f jobs/send_messages.sh ]; then
-    mkdir jobs
+    mkdir -p jobs
     echo "#!/bin/bash" > jobs/send_messages.sh
     echo "set -e" >> jobs/send_messages.sh
+fi
+
+# CHECK WB TOKEN VALIDITY
+FIRST_GROUP=$(jq -r '.[0].group' sku.json)
+FIRST_SKU=$(jq -r '[.[]|select(.group=="'$FIRST_GROUP'")][].sku[0].num' sku.json)
+REQUEST_STATUS=$(curl -s --location --request GET 'https://feedbacks-api.wildberries.ru/api/v1/feedbacks?isAnswered=false&take=5000&skip=0&nmId='$FIRST_SKU --header 'Authorization: '$WB_TOKEN --header 'Content-Type: application/json' | jq -r '.status')
+if  [ $REQUEST_STATUS == '401' ]; then
+    curl -s $TG_PROXY -X POST 'https://api.telegram.org/bot'$TG_BOT_TOKEN'/sendMessage' -d chat_id=$chat_id_1 -d text="Токен WB изменился! Требуется обновить токен в скрипте на сервере%0A$LK_NAME"
+    break
 fi
 
 for i in `seq 0 $(( $LENGTH - 1 ))`; do
@@ -39,14 +48,8 @@ for i in `seq 0 $(( $LENGTH - 1 ))`; do
     CURRENT_GROUP_LENGTH=$(jq -r '[.[]|select(.group=="'$CURRENT_GROUP'")][].sku[].num' sku.json | wc -l)
     for n in `seq 0 $(( $CURRENT_GROUP_LENGTH - 1 ))`; do
         CURRENT_SKU=$(jq -r '[.[]|select(.group=="'$CURRENT_GROUP'")][].sku['$n'].num' sku.json)
+        date +'%Y-%m-%d-%H-%M-%S'
         echo "n=$n; CURRENT_SKU=$CURRENT_SKU"
-
-        WB_TOKEN_CHANGE=$(curl -s --location --request GET 'https://feedbacks-api.wildberries.ru/api/v1/feedbacks?isAnswered=false&take=5000&skip=0&nmId='$CURRENT_SKU --header 'Authorization: '$WB_TOKEN --header 'Content-Type: application/json' | jq -r '.status')
-        if  [ $WB_TOKEN_CHANGE == '401' ]; then
-            curl -s -X POST 'https://api.telegram.org/bot'$TG_BOT_TOKEN'/sendMessage' -d chat_id=$chat_id_1 -d text="Токен WB изменился! Требуется обновить токен в скрипте на сервере%0A$LK_NAME"
-        break
-        fi
-
         CURRENT_FILEPATH=src/$CURRENT_SKU
         CURRENT_FILENAME=$DATETIME.json
         mkdir -p $CURRENT_FILEPATH
@@ -91,7 +94,6 @@ for i in `seq 0 $(( $LENGTH - 1 ))`; do
         else
             echo "This is not the first run"
             PREVIOUS_FILENAME=0-feedback-pool.json
-            echo PREVIOUS_FILENAME=$PREVIOUS_FILENAME
             FILES_ARE_DIFFERENT=$(jd -set $CURRENT_FILEPATH/$PREVIOUS_FILENAME $CURRENT_FILEPATH/$CURRENT_FILENAME | wc -l)
             echo FILES_ARE_DIFFERENT=$FILES_ARE_DIFFERENT
         fi
@@ -167,16 +169,11 @@ for i in `seq 0 $(( $LENGTH - 1 ))`; do
                     JSON_DATE_TIMESTAMP=$(date -ud "$JSON_DATE_GMT" +"%s")
                     ONE_MONTH_AGO_TIMESTAMP=$(date -ud "1 month ago" +"%s")
                     if [ $JSON_DATE_TIMESTAMP -gt $ONE_MONTH_AGO_TIMESTAMP ]; then
-                            # send messages from bot
+                            # collect curl commands in jobs/send_messages.sh
                             for k in `seq 1 $CHAT_NUM`; do
                                     current_chat_id=chat_id_$k
                                     if  [ "${!current_chat_id}" != "" ]; then
                                             echo "curl -s $TG_PROXY -X POST 'https://api.telegram.org/bot$TG_BOT_TOKEN/$CURL_METHOD' -d chat_id=${!current_chat_id} -d $CURL_DATA" >> jobs/send_messages.sh
-                                            bash jobs/send_messages.sh
-                                            CURL_OUTPUT=$(echo $?)
-                                            if [ $CURL_OUTPUT -eq 0 ]; then
-                                                    rm jobs/send_messages.sh
-                                            fi
                                             echo "--------------------"
                                     fi
                             done
@@ -199,3 +196,10 @@ for i in `seq 0 $(( $LENGTH - 1 ))`; do
         echo "--------------------"
     done
 done
+
+# SEND MESSAGES FROM BOT
+timeout 60 bash jobs/send_messages.sh
+CURL_OUTPUT=$(echo $?)
+if [ $CURL_OUTPUT -eq 0 ]; then
+    rm jobs/send_messages.sh
+fi
